5a99dcaf514386e0f756b3d741b58c90a17ca77a
Rasheed Donat <rdonat@nyc.yearup.org>
2018-10-30 15:47:22 -0400

    finalized

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-for (i = 99; i > 1; i--) {
+for (i = 99; i > 2; i--) {
   console.log(i, "crows on the wall.", i, "crows.", "1 fell down and became a wight.", i-1, "crows on the wall");
-  if (i == 2) {
+  if (i == 3) {
+    console.log ("2 crows on the wall. 1 single crow. 1 fell down and became a wight. 1 crow on the wall.");
     console.log ("1 crow on the wall. 1 single crow. It fell down and became a wight. There's no one left to defend Westeros now.");
     console.log("");
   }
