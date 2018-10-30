8147b7a9b00bbc0a983dd9183d7800587567ccf2
Rasheed Donat <rdonat@nyc.yearup.org>
2018-10-30 15:16:53 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+for (i = 99; i > 1; i--) {
+  console.log(i, "crows on the wall.", i, "crows.", "1 fell down and became a wight.", i-1, "crows on the wall");
+  if (i == 2) {
+    console.log ("1 crow on the wall. 1 single crow. It fell down and became a wight. There's no one left to defend Westeros now.");
+    console.log("");
+  }
+}
+
+// var prices = [33, 26, 99, 120, 12, 45];
+// var sum = 0;
+//
+// for (i = 0; i < prices.length; i++) {
+//   var sum = sum + prices[i];
+// }
+//   console.log("The sum is: ", sum);
